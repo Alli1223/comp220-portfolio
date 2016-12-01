7185dead956c39c2915e710bfff22f20b43c9159
Alastair Rayner <alli0@me.com>
2016-12-01 15:41:04 +0000

    Removed messy code

--- Portfolio/COMP220/04_Cube/Terrain.cpp
+++ Portfolio/COMP220/04_Cube/Terrain.cpp
@@ -63,6 +63,7 @@ void Terrain::generateTerrain(Mesh& groundTexture, Mesh& snowTexture)
 			/* TODO: Calculate neighbouring voxels and remove unnessary triangles
 			*/
 			
+			
 			// Place the voxel
 			placeCube(groundTexture, snowTexture, voxelPos, lastVoxelPos);
 			lastVoxelPos = voxelPos;
@@ -99,99 +100,17 @@ void Terrain::placeCube(Mesh& grassMesh, Mesh& mountainMesh, glm::vec3& voxelPos
 		glm::vec3 g(voxelPosition.x + voxelSize, voxelPosition.y - voxelSize, voxelPosition.z - voxelSize);
 		glm::vec3 h(voxelPosition.x + voxelSize, voxelPosition.y - voxelSize, voxelPosition.z + voxelSize);
 
-		glm::vec3 lasta(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z + voxelSize);
-		glm::vec3 lastb(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z + voxelSize);
-		glm::vec3 lastc(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 lastd(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 laste(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z + voxelSize);
-		glm::vec3 lastf(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 lastg(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 lasth(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z + voxelSize);
+		point.push_back(a);
+		point.push_back(b);
+		point.push_back(c);
+		point.push_back(d);
+		point.push_back(e);
+		point.push_back(f);
+		point.push_back(g);
+		point.push_back(h);
 
-		// Fix rounding errors
-		// Remove squares behind camera
-		a.x = round(a.x); a.y = round(a.y); a.z = round(a.z);
-		b.x = round(b.x); b.y = round(b.y); b.z = round(b.z);
-		c.x = round(c.x); c.y = round(c.y); c.z = round(c.z);
-		d.x = round(d.x); d.y = round(d.y); d.z = round(d.z);
-		e.x = round(e.x); e.y = round(e.y); e.z = round(e.z);
-		f.x = round(f.x); f.y = round(f.y); f.z = round(f.z);
-		g.x = round(g.x); g.y = round(g.y); g.z = round(g.z);
-		h.x = round(h.x); h.y = round(g.y); h.z = round(g.z);
-
-		lasta.x = round(lasta.x); lasta.y = round(lasta.y); lasta.z = round(a.z);
-		lastb.x = round(lastb.x); lastb.y = round(lastb.y); lastb.z = round(b.z);
-		lastc.x = round(lastc.x); lastc.y = round(lastc.y); lastc.z = round(c.z);
-		lastd.x = round(lastd.x); lastd.y = round(lastd.y); lastd.z = round(d.z);
-		laste.x = round(laste.x); laste.y = round(laste.y); laste.z = round(e.z);
-		lastf.x = round(lastf.x); lastf.y = round(lastf.y); lastf.z = round(f.z);
-		lastg.x = round(lastg.x); lastg.y = round(lastg.y); lastg.z = round(g.z);
-		lasth.x = round(lasth.x); lasth.y = round(lasth.y); lasth.z = round(g.z);
 		
 
-		if (a == lasta)
-		{
-			a = null;
-			point.push_back(a);
-		}
-		else
-			point.push_back(a);
-
-		if (b == lastb)
-		{
-			b = null;
-			point.push_back(b);
-		}
-		else
-			point.push_back(b);
-
-		if (c == lastc)
-		{
-			c = null;
-			point.push_back(c);
-		}
-		else
-			point.push_back(c);
-
-		if (d == lastd)
-		{
-			d = null;
-			point.push_back(d);
-		}
-		else
-			point.push_back(d);
-
-		if (e == laste)
-		{
-			e = null;
-			point.push_back(e);
-		}
-		else
-			point.push_back(e);
-
-		if (f == lastf)
-		{
-			f = null;
-			point.push_back(f);
-		}
-		else
-			point.push_back(f);
-
-		if (g == lastg)
-		{
-			g = null;
-			point.push_back(g);
-		}
-		else
-			point.push_back(g);
-
-		if (h == lasth)
-		{
-			h = null;
-			point.push_back(h);
-		}
-		else
-			point.push_back(h);
 
 		grassMesh.addCubeFromFace(point, colour);
 	}
@@ -211,79 +130,14 @@ void Terrain::placeCube(Mesh& grassMesh, Mesh& mountainMesh, glm::vec3& voxelPos
 		// Offset the mountain terrain to be lighter at the top
 		glm::vec3 colour(voxelPosition.y / 50, voxelPosition.y / 50, voxelPosition.y / 50);
 
-		glm::vec3 lasta(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z + voxelSize);
-		glm::vec3 lastb(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z + voxelSize);
-		glm::vec3 lastc(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 lastd(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y + voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 laste(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z + voxelSize);
-		glm::vec3 lastf(lastVoxelPosition.x - voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 lastg(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z - voxelSize);
-		glm::vec3 lasth(lastVoxelPosition.x + voxelSize, lastVoxelPosition.y - voxelSize, lastVoxelPosition.z + voxelSize);
-
-		if (a == lasta)
-		{
-			a = null;
-			point.push_back(a);
-		}
-		else
-			point.push_back(a);
-
-
-		if (b == lastb)
-		{
-			b = null;
-			point.push_back(b);
-		}
-		else
-			point.push_back(b);
-
-		if (c == lastc)
-		{
-			c = null;
-			point.push_back(c);
-		}
-		else
-			point.push_back(c);
-
-		if (d == lastd)
-		{
-			d = null;
-			point.push_back(d);
-		}
-		else
-			point.push_back(d);
-
-		if (e == laste)
-		{
-			e = null;
-			point.push_back(e);
-		}
-		else
-			point.push_back(e);
-
-		if (f == lastf)
-		{
-			f = null;
-			point.push_back(f);
-		}
-		else
-			point.push_back(f);
-
-		if (g == lastg)
-		{
-			g = null;
-			point.push_back(g);
-		}
-		else
-			point.push_back(g);
-
-		if (h == lasth)
-		{
-			h = null;
-			point.push_back(h);
-		}
-		else
-			point.push_back(h);
+		point.push_back(a);
+		point.push_back(b);
+		point.push_back(c);
+		point.push_back(d);
+		point.push_back(e);
+		point.push_back(f);
+		point.push_back(g);
+		point.push_back(h);
 
 		mountainMesh.addCubeFromFace(point, colour);
 	}
